d8cd340dbcac40d7c5ea018ae9bb8db4702ea987
ce401-06 <ce401-06@ce401-06-HP>
2015-01-26 13:20:18 +0900

    start

--- SharedProject/src/java/EunHye.java
+++ SharedProject/src/java/EunHye.java
@@ -3,7 +3,6 @@ public class EunHye {
 
 	public static void main(String[] args) {
 		System.out.println("은혜 Hi");
-
 	}
 
 }
---
+++ SharedProject/src/java/hi.java
@@ -0,0 +1,8 @@
+
+public class hi {
+
+	public static void main(String[] args) {
+		System.out.println("니가 있던 소오!~빠");
+	}
+
+}
